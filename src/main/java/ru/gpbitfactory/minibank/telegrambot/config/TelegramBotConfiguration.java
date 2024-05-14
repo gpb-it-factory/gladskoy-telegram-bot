fb5f0bc7a0afc940ec1d76ca2d744614bc9cdd9c
Aleksandr Gladskoy <a.gladskoy@gmail.com>
2024-05-14 17:20:07 +0300

    Задание 3. Внесены изменения в конфиг для более удобного добавления новых команд

--- src/main/java/ru/gpbitfactory/minibank/telegrambot/config/TelegramBotConfiguration.java
+++ src/main/java/ru/gpbitfactory/minibank/telegrambot/config/TelegramBotConfiguration.java
@@ -5,10 +5,9 @@ import lombok.SneakyThrows;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
-import ru.gpbitfactory.minibank.telegrambot.command.PingCommand;
-import ru.gpbitfactory.minibank.telegrambot.command.StartCommand;
 import ru.gpbitfactory.minibank.telegrambot.handler.CommandsHandler;
 
 import java.util.List;
@@ -21,19 +20,15 @@ public class TelegramBotConfiguration {
     private final TelegramBotProperties properties;
 
     @Bean
-    public LongPollingUpdateConsumer longPollingUpdateConsumer() {
-        var commands = List.of(
-                new StartCommand(),
-                new PingCommand()
-        );
+    public LongPollingUpdateConsumer longPollingUpdateConsumer(List<BotCommand> commands) {
         return new CommandsHandler(properties.getToken(), properties.getName(), commands);
     }
 
     @Bean
     @SneakyThrows
-    public TelegramBotsLongPollingApplication telegramBotsLongPollingApplication() {
+    public TelegramBotsLongPollingApplication telegramBotsLongPollingApplication(LongPollingUpdateConsumer consumer) {
         var botsApplication = new TelegramBotsLongPollingApplication();
-        botsApplication.registerBot(properties.getToken(), longPollingUpdateConsumer());
+        botsApplication.registerBot(properties.getToken(), consumer);
         return botsApplication;
     }
 }
