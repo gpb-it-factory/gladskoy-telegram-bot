615c018dacd50c2f596249b879a1b85c3a0edcff
Aleksandr Gladskoy <a.gladskoy@gmail.com>
2024-06-13 23:07:14 +0300

    Обновлён контракт Middle Service до версии 0.0.2 (#38)
    * Обновлён контракт Middle Service до версии 0.0.2
    
    * Команда /register переведена на работу с новой версией операции POST /v2/clients

--- src/main/java/ru/gpbitfactory/minibank/telegrambot/TelegramBotApplication.java
+++ src/main/java/ru/gpbitfactory/minibank/telegrambot/TelegramBotApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("ru.gpbitfactory.minibank.middle.api")
+@EnableFeignClients
 public class TelegramBotApplication {
 
     public static void main(String[] args) {
